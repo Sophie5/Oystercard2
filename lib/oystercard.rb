2cf84ba50dd32d3d653c228c68c2f48b09ef9969
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-02-20 18:09:40 +0000

    #deduct method added to Oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,21 @@
 class Oystercard
 
   DEFAULT_BALANCE=0
+  MAXIMUM_BALANCE=90
 
   attr_reader :balance
 
   def initialize(set_balance = DEFAULT_BALANCE)
     @balance = set_balance
+  end
+
+  def top_up(amount)
+    raise "Failed! Your card limit is #{Oystercard::MAXIMUM_BALANCE}." if (amount + balance) > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,30 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+  it 'has a default balance of zero' do
+    expect(Oystercard::DEFAULT_BALANCE).to eq(0)
   end
+
+  describe '#top_up' do
+
+    it 'has a limit' do
+      subject.top_up Oystercard::MAXIMUM_BALANCE
+      expect { subject.top_up 1 }.to raise_error "Failed! Your card limit is #{Oystercard::MAXIMUM_BALANCE}."
+    end
+
+    it 'can top up the balance' do
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
+  end
+
+  describe '#deduct a fare' do
+
+    it 'can deduct value from fare' do
+      subject.top_up(20)
+      expect {subject.deduct 1 }.to change {subject.balance}.by -1
+    end
+
+  end
+
 
 end
